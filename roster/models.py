97cbcf6b425c109b61f874ec924371853b2737c5
cjredmond <connor.redmond@gmail.com>
2017-02-08 15:44:18 -0500

    divided player class to Packers, Draft, FreeAgent classes

--- roster/models.py
+++ roster/models.py
@@ -11,7 +11,17 @@ class Player(models.Model):
     position = models.CharField(max_length=2, choices=POSITION)
     cap_hit = models.IntegerField()
     cut_savings = models.IntegerField(null=True,blank=True)
+
+class FreeAgent(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    position = models.CharField(max_length=2, choices=POSITION)
+    cap_hit = models.IntegerField()
     on_team = models.BooleanField()
-    contract_up = models.BooleanField()
-    draft = models.BooleanField()
-    draft_round = models.IntegerField(null=True,blank=True)
+
+class DraftPlayer(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    position = models.CharField(max_length=2, choices=POSITION)
+    cap_hit = models.IntegerField()
+    draft_round = models.IntegerField()
